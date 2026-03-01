9ca670df5607be50905133768306ec7c438346eb
Elangoit001 <elangopoomi@gmail.com>
2026-03-01 11:52:25 +0530

    Update config for dynamic API URL (Local vs Public)

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -1,2 +1,4 @@
-const API_BASE_URL = `http://${window.location.hostname}:5000/api`;
+const API_BASE_URL = window.location.hostname === 'localhost'
+    ? `http://localhost:5000/api`
+    : `https://witty-tables-check.loca.lt/api`;
 export default API_BASE_URL;
