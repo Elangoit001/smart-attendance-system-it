61a3b0c1adffe7ec3c062c50a8698f5b3b501d3e
Elangoit001 <elangopoomi@gmail.com>
2026-03-01 11:58:30 +0530

    Update API URL for restriction-free public access

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -1,4 +1,4 @@
 const API_BASE_URL = window.location.hostname === 'localhost'
     ? `http://localhost:5000/api`
-    : `https://witty-tables-check.loca.lt/api`;
+    : `https://8c63e339b67f9be0-157-51-72-89.serveousercontent.com/api`;
 export default API_BASE_URL;
